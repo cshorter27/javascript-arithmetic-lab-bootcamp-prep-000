6220db728b7cc4c53f72912eb9e908e0b1a4f61f
Charles Shorter Jr <c4short27@gmail.com>
2019-04-27 03:47:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -32,21 +32,12 @@ function makeInt(n){
 }
 
 function preserveDecimal(n) {
-  return preserveDecimal(n)
+  return preserveDecimal()
 }  
 
  function preserveDecimal(n) {
-  return preserveDecimal(n)
+  return preserveDecimal(NaN)
 }  
 
 
-describe('preserveDecimal(n)', function() {
-  it('preserves n\'s decimals (it parses n as a floating point number) and returns the parsed number', function() {
-    expect(preserveDecimal('2.222')).toBe(2.222)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(preserveDecimal('sldkjflksjf'))).toEqual(true)
-  })
-})
 
